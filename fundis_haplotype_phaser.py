53e48f40e56fd0f0ef70470da878e653f356eeab
Ian M. B <99409346+iPsychonaut@users.noreply.github.com>
2023-07-22 21:26:31 -0700

    Update fundis_haplotype_phaser.py
    Minor changes to check functionality.
    
    Signed-off-by: Ian M. B. <99409346+iPsychonaut@users.noreply.github.com>

--- fundis_haplotype_phaser.py
+++ fundis_haplotype_phaser.py
@@ -20,6 +20,7 @@ import math
 import queue
 import pysam
 import argparse
+import platform
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
@@ -73,137 +74,124 @@ def analyze_ngspeciesid_folder(ngspeciesid_dir):
     if not fasta_files:
         return
     
-    # # Extract data from all fasta files
-    # data = []
-    # for fasta_file in fasta_files:
-    #     data.extend(extract_data_from_fasta(fasta_file))
-    
-    # # Create a dataframe from the extracted data
-    # df = pd.DataFrame(data, columns=['sequence_id', 'reads_count'])
-    
-    # # Find the index of the sequence with the maximum reads
-    # index_max_reads = df['reads_count'].idxmax()
-    
-    # # Get the input fasta file
-    # input_fasta = fasta_files[index_max_reads]
-    
-    # Run through each file 
-    for input_fastq in fasta_files:
-        if os.path.isdir(base_folder):
-            print(f"PASS: Skipping haplotype split, {base_folder} already exists\n")
-        else:
-            # # Define the base folder
-            base_folder = input_fasta.rsplit('.', 1)[0]
-            os.makedirs(base_folder, exist_ok=True)  # add exist_ok=True
-            os.chdir(base_folder)
+    # Extract data from all fasta files
+    for input_fasta in fasta_files:
+        # Define the base folder
+        base_folder = input_fasta.rsplit('.', 1)[0]
+        
+        # if os.path.isdir(base_folder):
+        #     print(f"PASS: Skipping haplotype split, {base_folder} already exists\n")
+        # else:
+        os.makedirs(base_folder, exist_ok=True)  # add exist_ok=True
+        os.chdir(base_folder)
 
-            input_fastq = input_fasta.replace('consensus_reference','reads_to_consensus').replace('.fasta','.fastq')
-            
-            fasta_basename = os.path.basename(input_fasta)
-            
-            sam_file = os.path.join(base_folder, fasta_basename.replace('.fasta','.sam'))
-            
-            bam_file = os.path.join(base_folder, sam_file.replace('.sam','.bam'))
-            
-            sorted_bam = os.path.join(base_folder, bam_file.replace('.bam','_sorted.bam'))
-                                 
-            bcf_file = os.path.join(base_folder, sorted_bam.replace('_sorted.bam','.bcf'))
-            
-            pileup_output = os.path.join(base_folder, bcf_file.replace(".bcf", "_pileup.bcf"))
-            
-            vcf_file = os.path.join(base_folder, bcf_file.replace(".bcf",'.vcf'))
-            
-            phased_vcf = os.path.join(base_folder, vcf_file.replace('.vcf','.phased.vcf'))
-            
-            haplotype_fasta = os.path.join(base_folder, phased_vcf.replace('.phased.vcf','_haplotypes.fasta'))
-            
-            # Step 1: Use BWA to align your FASTQ file to your reference FASTA file
-            # First, index the reference genome
-            subprocess.run(["bwa", "index", input_fasta], check=True)
-            
-            # Then, align the reads to the reference
-            subprocess.run(["bwa", "mem", input_fasta, input_fastq, "-o", sam_file], check=True)
-            
-            # Step 2: Convert SAM to BAM using SAMtools
-            subprocess.run(["samtools", "view", "-S", "-b", sam_file, "-o", bam_file], check=True)
-            
-            # Step 3: Sort the BAM file
-            subprocess.run(["samtools", "sort", bam_file, "-o", sorted_bam], check=True)
-            
-            # Add this line to create the index file for the sorted BAM file:
-            subprocess.run(["samtools", "index", sorted_bam], check=True)
-            
-            # Step 4: Call variants with bcftools to get a VCF file
-            
-            subprocess.run(["bcftools", "mpileup", "-Ou", "-f", input_fasta, sorted_bam, "-o", pileup_output], check=True)
-            subprocess.run(["bcftools", "call", "-mv", "-Ob", "-o", bcf_file, pileup_output], check=True)
-            subprocess.run(["bcftools", "view", bcf_file, "-Ov", "-o", vcf_file], check=True)
-            
-            # Step 5: Use Whatshap to find haplotypes
-            subprocess.run(["whatshap", "phase", "-o", phased_vcf, "--reference", input_fasta, vcf_file, sorted_bam], check=True)
-            
-            # Step 6: Parse the output VCF file and generate sequences based on the original input FASTA        
-            # First, let's load the original reference FASTA file
-            original_seq_record = SeqIO.read(input_fasta, "fasta")
-            original_seq = str(original_seq_record.seq)
-            
-            # Then, let's parse the VCF file using pysam
-            vcf = pysam.VariantFile(phased_vcf)
-            
-            new_sequences = []
-            
-            # Let's iterate over each variant in the VCF file
-            for variant in vcf:
-                # We'll create a new sequence for each allele
-                for allele in variant.alleles:
-                    if allele != variant.ref:  # We don't need to create a new sequence for the reference allele
-                        new_seq_str = original_seq[:variant.pos-1] + allele + original_seq[variant.pos:]
-                        record_id = variant.id if variant.id is not None else f"pos_{variant.pos}_allele_{allele}"
-                        new_seq_record = SeqRecord(Seq(new_seq_str), id=record_id, description="")
-                        new_sequences.append(new_seq_record)
-            
-            # Finally, let's write the new sequences to a new FASTA file
-            SeqIO.write(new_sequences, haplotype_fasta, "fasta")
-            
-            # Initialize an empty list to store all haplotype sequences
-            haplotype_sequences = []
+        input_fastq = input_fasta.replace('consensus_reference','reads_to_consensus').replace('.fasta','.fastq')
         
-            # Use glob to find all haplotype fasta files
-            haplotype_files = glob.glob(os.path.join(base_folder, '**', '*haplotypes.fasta'), recursive=True)
+        fasta_basename = os.path.basename(input_fasta)
         
-            # Take Haplotype File and save individual fasta files for each haplotype that consist of a single degenerate sequence
-            for haplotype_file in haplotype_files:
-                for i, record in enumerate(SeqIO.parse(haplotype_file, "fasta")):
-                    # Create a new fascondata file for each sequence
-                    individual_fasta_path = os.path.join(os.path.dirname(haplotype_file), f"{record.id}_individual.fasta")
-                    with open(individual_fasta_path, "w") as individual_fasta_file:
-                        SeqIO.write(record, individual_fasta_file, "fasta")
-                            
-        # Remove files that end in '.bcf', '_sorted.vcf', '.amb', '.ann', '.bwt', '.pac', '.sa', and '.bai' from the base folder
-        exts = ['.bcf', '_sorted.vcf', '.amb', '.ann', '.bwt', '.pac', '.sa', '.bai']
+        sam_file = os.path.join(base_folder, fasta_basename.replace('.fasta','.sam'))
         
-        # Iterate over each extension
-        for ext in exts:
-            # Use glob to find all files with the current extension, including subfolders
-            file_list = glob.glob(os.path.join(ngspeciesid_dir, '**', '*' + ext), recursive=True)
+        bam_file = os.path.join(base_folder, sam_file.replace('.sam','.bam'))
         
-            # Iterate over each file and remove it
-            for file_path in file_list:
-                os.remove(file_path)
-        return True
+        sorted_bam = os.path.join(base_folder, bam_file.replace('.bam','_sorted.bam'))
+                             
+        bcf_file = os.path.join(base_folder, sorted_bam.replace('_sorted.bam','.bcf'))
+        
+        pileup_output = os.path.join(base_folder, bcf_file.replace(".bcf", "_pileup.bcf"))
+        
+        vcf_file = os.path.join(base_folder, bcf_file.replace(".bcf",'.vcf'))
+        
+        phased_vcf = os.path.join(base_folder, vcf_file.replace('.vcf','.phased.vcf'))
+        
+        haplotype_fasta = os.path.join(base_folder, phased_vcf.replace('.phased.vcf','_haplotypes.fasta'))
+        
+        # Step 1: Use BWA to align your FASTQ file to your reference FASTA file
+        # First, index the reference genome
+        subprocess.run(["bwa", "index", input_fasta], check=True)
+        
+        # Then, align the reads to the reference
+        subprocess.run(["bwa", "mem", input_fasta, input_fastq, "-o", sam_file], check=True)
+        
+        # Step 2: Convert SAM to BAM using SAMtools
+        subprocess.run(["samtools", "view", "-S", "-b", sam_file, "-o", bam_file], check=True)
+        
+        # Step 3: Sort the BAM file
+        subprocess.run(["samtools", "sort", bam_file, "-o", sorted_bam], check=True)
+        
+        # Add this line to create the index file for the sorted BAM file:
+        subprocess.run(["samtools", "index", sorted_bam], check=True)
+        
+        # Step 4: Call variants with bcftools to get a VCF file
+        
+        subprocess.run(["bcftools", "mpileup", "-Ou", "-f", input_fasta, sorted_bam, "-o", pileup_output], check=True)
+        subprocess.run(["bcftools", "call", "-mv", "-Ob", "-o", bcf_file, pileup_output], check=True)
+        subprocess.run(["bcftools", "view", bcf_file, "-Ov", "-o", vcf_file], check=True)
+        
+        # Step 5: Use Whatshap to find haplotypes
+        subprocess.run(["whatshap", "phase", "-o", phased_vcf, "--reference", input_fasta, vcf_file, sorted_bam], check=True)
+        
+        # Step 6: Parse the output VCF file and generate sequences based on the original input FASTA        
+        # First, let's load the original reference FASTA file
+        original_seq_record = SeqIO.read(input_fasta, "fasta")
+        original_seq = str(original_seq_record.seq)
+        
+        # Then, let's parse the VCF file using pysam
+        vcf = pysam.VariantFile(phased_vcf)
+        
+        new_sequences = []
+        
+        # Let's iterate over each variant in the VCF file
+        for variant in vcf:
+            # We'll create a new sequence for each allele
+            for allele in variant.alleles:
+                if allele != variant.ref:  # We don't need to create a new sequence for the reference allele
+                    new_seq_str = original_seq[:variant.pos-1] + allele + original_seq[variant.pos:]
+                    record_id = variant.id if variant.id is not None else f"pos_{variant.pos}_allele_{allele}"
+                    new_seq_record = SeqRecord(Seq(new_seq_str), id=record_id, description="")
+                    new_sequences.append(new_seq_record)
+        
+        # Finally, let's write the new sequences to a new FASTA file
+        SeqIO.write(new_sequences, haplotype_fasta, "fasta")
+        
+        # Initialize an empty list to store all haplotype sequences
+        haplotype_sequences = []
+    
+        # Use glob to find all haplotype fasta files
+        haplotype_files = glob.glob(os.path.join(base_folder, '**', '*haplotypes.fasta'), recursive=True)
+    
+        # Take Haplotype File and save individual fasta files for each haplotype that consist of a single degenerate sequence
+        for haplotype_file in haplotype_files:
+            for i, record in enumerate(SeqIO.parse(haplotype_file, "fasta")):
+                # Create a new fascondata file for each sequence
+                individual_fasta_path = os.path.join(os.path.dirname(haplotype_file), f"{record.id}_individual.fasta")
+                with open(individual_fasta_path, "w") as individual_fasta_file:
+                    SeqIO.write(record, individual_fasta_file, "fasta")
+                        
+    # Remove files that end in '.bcf', '_sorted.vcf', '.amb', '.ann', '.bwt', '.pac', '.sa', and '.bai' from the base folder
+    exts = ['.bcf', '_sorted.vcf', '.amb', '.ann', '.bwt', '.pac', '.sa', '.bai']
+    
+    # Iterate over each extension
+    for ext in exts:
+        # Use glob to find all files with the current extension, including subfolders
+        file_list = glob.glob(os.path.join(ngspeciesid_dir, '**', '*' + ext), recursive=True)
+    
+        # Iterate over each file and remove it
+        for file_path in file_list:
+            os.remove(file_path)
+    return True
  
 # Main entry point of the script
 def main(args):
     # Define the main directory based on the argument or use a default value
     input_dir = args.input_dir if args.input_dir else '/mnt/e/Fundis/NGSpeciesID-20230719T211158Z-001/NGSpeciesID'
-    percent_system_use = float(args.percent_system_use/100) if args.percent_system_use else 0.8
+    percent_system_use = float(args.percent_system_use)/100 if args.percent_system_use else 0.8
     
     # Get the number of CPUs and calculate the number of threads
     num_cpus = multiprocessing.cpu_count()
     cpu_threads = int(math.floor(num_cpus * percent_system_use))
     
-    # Get a list of all folders in the main directory, excluding folders named '__Summary__' or 'Summary2'
-    folder_list = [f.path for f in os.scandir(input_dir) if f.is_dir() and '__Summary__' not in f.path and 'Summary2' not in f.path]
+    # Get a list of all folders in the main directory, excluding folders named '__Summary__' or '_Summary'
+    folder_list = [f.path for f in os.scandir(input_dir) if f.is_dir() and '__Summary__' not in f.path and '_Summary' not in f.path]
     
     try:
         # Initialize a multiprocessing pool and process all folders
