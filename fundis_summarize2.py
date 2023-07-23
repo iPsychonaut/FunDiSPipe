0e73b99c27671abce38d5880f38bc498b3c66a05
Ian M. B <99409346+iPsychonaut@users.noreply.github.com>
2023-07-22 21:25:14 -0700

    Update fundis_summarize2.py
    Complete refactor of code from scratch. Include movement of all files, renaming, and summarizing (including haplotypes).
    
    Signed-off-by: Ian M. B. <99409346+iPsychonaut@users.noreply.github.com>

--- fundis_summarize2.py
+++ fundis_summarize2.py
@@ -7,187 +7,164 @@ Created on Wed Jul 19 2023
 This is a module intended to be used as a part of a pipeline.
 
 This can be used individually by calling the command:
-    python /path/to/fundis_summarize.py -s /path/to/source_folder -p 80
-    python /path/to/fundis_summarize.py --source_folder /path/to/source_folder --percent_system_use 80
+    python /path/to/fundis_summarize.py -i /path/to/input_dir -p 80
+    python /path/to/fundis_summarize.py --input_dir /path/to/input_dir --percent_system_use 80
 """
+from Bio import SeqIO
+import pandas as pd
 import os
-import re
 import glob
-import csv
 import shutil
 from tqdm import tqdm
 import argparse
-import math
-import multiprocessing
-from pathlib import Path
 
-# Function to determine which Operating System the code is being executed in
-def check_os():
-    global environment_dir
-    global environment_cmd_prefix
-    # Determine the operating system in use
-    # os.name will return 'posix', 'nt', or 'java' 
-    os_name = os.name  
-    # platform.system() will return 'Linux', 'Windows', 'Java', etc.
-    platform_system = platform.system()  
+# Function to take in a folder containined processed NGSequenceID folders and generate a summary folder for MycoMap
+def mycomap_summarize_ngsid_dir(ngsid_dir):
+    print('\nGenerating MycoMap summary folder for {ngsid_dir}...')
+    # Create the summary and FASTQ directories if they don't exist
+    summary_dir = ngsid_dir.replace("_minibar_NGSID","_Summary")
     
-    # If the operating system is Windows (identified by 'nt' from os.name or 'Windows' from platform.system())
-    if os_name == 'nt' or platform_system == 'Windows':
-        # Set the working directory to "E:" (or whatever drive letter is appropriate for your Windows system)
-        environment_dir = "E:"
-        # For running Linux commands in Windows Subsystem for Linux (WSL), prefix the command with "wsl " 
-        environment_cmd_prefix = "wsl "
+    # Create pandas DataFrame for sequence information
+    stats_df = pd.DataFrame(columns=['Filename', 'Length', 'Reads in Consensus', 'Multiple'])
     
-    # If the operating system is Linux (identified by 'posix' from os.name or 'Linux' from platform.system())
-    elif os_name == 'posix' or platform_system == 'Linux':
-        # Set the working directory to "/mnt/e" (or whatever the corresponding path is in your Linux system)
-        environment_dir = "/mnt/e"  
+    # Get all directories in the ngsid_dir
+    sample_dirs = [d for d in os.listdir(ngsid_dir) if os.path.isdir(os.path.join(ngsid_dir, d))]
     
-    else:
-        # If the operating system is neither Windows nor Linux, raise an Exception
-        raise Exception("ERROR: OS NOT TESTED WITH THIS CODE.")
+    fastq_dir = f"{summary_dir}/FASTQ Files"
+    os.makedirs(summary_dir, exist_ok=True)
+    os.makedirs(fastq_dir, exist_ok=True)
     
-    # Print out the detected operating system and the determined environment directory
-    print(f'Operating System: {platform_system}')
-    print(f'Environment Directory: {environment_dir}')
-    return environment_dir
-
-# Function to update the name of a given fasta file to include the total supporting reads 
-def update_fasta_file(file, parent_folder):
-    with open(file, 'r') as f:
-        lines = f.readlines()
-
-    first_line = lines.pop(0)
-    length = re.search(r'total_supporting_reads_\d+:\d\.\d-(\d+)\.\d', first_line).group(1)
-    reads = re.search(r'total_supporting_reads_(\d+):\d\.\d-\d+\.\d', first_line).group(1)
-
-    lines.insert(0, f'>{parent_folder}\n')
-
-    with open(file, 'w') as f:
-        f.writelines(lines)
-
-    return length, reads
-
-
-def get_fasta_reads(fasta_file):
-    with open(fasta_file, 'r') as f:
-        first_line = f.readline()
-        reads = re.search(r'total_supporting_reads_(\d+):\d\.\d-\d+\.\d', first_line).group(1)
-    return reads
-
-
-def process_medaka_folder(folder, medaka_id):
-    fasta_file = os.path.join(folder, 'consensus.fasta')
-
-    if os.path.exists(fasta_file):
-        p = Path(fasta_file)
-        parent_folder = p.parent.parent.name
-        parent_folder_name = re.sub(r'^sample_', '', parent_folder)
-        reads = get_fasta_reads(fasta_file)
-        target_fasta_file = os.path.join(summary_folder, f'{parent_folder_name}-{medaka_id}-RiC{reads}.fasta')
-        shutil.copy(fasta_file, target_fasta_file)
-        length, reads = update_fasta_file(target_fasta_file, f'{parent_folder_name}-{medaka_id}')
-
-        p = Path(folder)
-        medaka_parent_folder = str(p.parent)
-        fastq_file_index = 1
-        for fastq_file in sorted(glob.glob(f'{medaka_parent_folder}/reads_to_consensus_[0-9]*.fastq')):
-            if os.path.exists(fastq_file):
-                target_fastq_file = os.path.join(summary_folder, 'FASTQ Files', f'{parent_folder_name}-{fastq_file_index}.fastq')
-                shutil.copy(fastq_file, target_fastq_file)
-                fastq_file_index += 1
-    
-        return f'{parent_folder_name}-{medaka_id}', length, reads
-    else:
-        return None, None, None
-
-def process_folder(folder):
-    medaka_id = 1
-    stats = []
-    items = sorted(glob.iglob(folder + '/*', recursive=False))
-    for item in items:
-        if not os.path.isfile(item):
-            p = Path(item)
-            folder_name = p.name
-            if folder_name.startswith('medaka'):
-                filename, length, reads = process_medaka_folder(item, medaka_id)
-                stats.append([filename, length, reads, medaka_id])
-                medaka_id += 1
-    return stats
-
-def merge_fasta_files(folder, output_file):
-    with open(os.path.join(folder, output_file), 'w') as f_out:
-        for fasta_file in sorted(glob.glob(f'{folder}/*.fasta')):
-            with open(fasta_file, 'r') as f:
-                f_out.write(f.read())
-
-def generate_summary(stats, writer):
-
-    summary_totals = {'Total Unique Samples': 0,
-                      'Total Consensus Sequences': 0,
-                      'Total Reads in Consensus Sequences': 0}
-    additional_stats = []
-    counted_samples = {}
-
-    for row in stats:
-        filename = row[0]
-        if filename is not None:
-            if row[2] is not None:
-                reads = int(row[2])
-            else:
-                reads = 0
-            if filename.count('ONT') > 1:
-                additional_stats.append(row)
-            else:
-                writer.writerow(row)
-                unique_part = re.search(r'^([^-]+)', filename).group(1)
-                if unique_part not in counted_samples:
-                    summary_totals['Total Unique Samples'] += 1
-                    counted_samples[unique_part] = 1
-            summary_totals['Total Consensus Sequences'] += 1
-            summary_totals['Total Reads in Consensus Sequences'] += reads
-
-    writer.writerow([])
-    writer.writerow(['Total Unique Samples', summary_totals['Total Unique Samples']])
-    writer.writerow(['Total Consensus Sequences', summary_totals['Total Consensus Sequences']])
-    writer.writerow(['Total Reads in Consensus Sequences', summary_totals['Total Reads in Consensus Sequences']])
-    writer.writerow([])
-    for row in additional_stats:
-        writer.writerow(row)
-
-def main(args):
-    percent_system_use = float(args.percent_system_use/100) if args.percent_system_use else 0.8
-    source_folder = args.source_folder if args.source_folder else os.path.dirname(os.path.realpath(__file__))
-    
-    # Name summary folder according to original source_folder but adding _Summary
-    source_folder_split = '/'.join(source_folder.split('/')[:-1])
-    source_folder_base = source_folder.split('/')[-1]    
-    summary_folder = f"{source_folder_split}/{source_folder_base}_Summary"
-    
-    if not os.path.exists(summary_folder):
-        os.mkdir(summary_folder)
-    if not os.path.exists(os.path.join(summary_folder, 'FASTQ Files')):
-        os.mkdir(os.path.join(summary_folder, 'FASTQ Files'))
-    
-    with open(os.path.join(summary_folder, 'summary.txt'), 'w', encoding='utf-8') as f_out:
-        writer = csv.writer(f_out, delimiter='\t', lineterminator='\n')
-        writer.writerow(['Filename', 'Length', 'Reads in Consensus', 'Multiple'])
+    # MAKE LOOK THROUGH SAMPLE DIR IN SAMPLE DIRS
+    for current_sample_dir in tqdm(sample_dirs):
+        #current_sample_dir = "E:/Fundis/TEST/sample_HS_ONT03_03_35-HAY-F-001900-iNat155234876-Agaricomycetes_NGSequenceID"
+        current_sample_dir = f'{ngsid_dir}/{current_sample_dir}'
+        base_name = current_sample_dir.split("sample_")[-1]
         
-        try:
-            with multiprocessing.Pool(multiprocessing.cpu_count()) as pool:
-                folders = sorted(glob.iglob(source_folder + '/*', recursive=False))
-                results = [pool.apply_async(process_folder, args=(folder,)) for folder in folders]
-                stats = [p.get() for p in results]
-        except Exception as e:
-            print(f"An error occurred during multiprocessing: {e}")
-            raise  # re-raise the exception after handling it
-        else:
-            generate_summary(stats, writer)
-    
-    merge_fasta_files(summary_folder, 'summary.fasta')
-    print('PASS: Successfully summarized NGSpeciesID folder\n')
+        # get all directories in the path that match the pattern
+        consensus_dirs = [entry.path for entry in os.scandir(current_sample_dir) if entry.is_dir() and entry.name.startswith('consensus_reference_') and any(os.scandir(entry.path))]
+        
+        consensus_dirs = [entry.replace("\\","/") for entry in consensus_dirs]
+        
+        consensus_fastq_list = []
+        
+        medaka_count = 1
+        
+        reads_in_consensus = 0
+        
+        for consensus_dir in consensus_dirs:
+            # Use glob to get a list of all files that match the pattern "_individual.fasta"
+            individual_fasta_files = glob.glob(f"{consensus_dir}/**/*_individual.fasta", recursive=True)
+            haplotype_count = len(individual_fasta_files)
+            
+            # Generate filenames for fasta and fastq associated with the current consensus
+            fasta_file = f'{consensus_dir}.fasta'
+            fastq_file = f'{consensus_dir}.fastq'
+            fastq_file = fastq_file.replace('consensus_reference','reads_to_consensus')
+            
+            # Get number of reads in consensus
+            for record in SeqIO.parse(fasta_file, "fasta"):
+                sequence_length = len(record.seq)
+                record_id = record.id
+                reads_in_consensus = record_id.split('total_supporting_reads_')[-1]
+            new_fastq_filename = base_name.replace('_NGSequenceID', f'-{medaka_count}-RiC{reads_in_consensus}.fastq')
+            new_fastq_filepath = os.path.join(fastq_dir, new_fastq_filename)        
+            shutil.move(fastq_file, new_fastq_filepath)
+            if haplotype_count == 1:
+                # Rename the consensus_reference_#.fasta in current_sample_dir to use the base_name but replace '_NGSequenceID' with '-{medaka_count}-RiC{reads_in_consensus}.fasta'
+                haplotype_letter = chr(65)
+                new_fasta_filename = base_name.replace('_NGSequenceID', f'-{medaka_count}-RiC{reads_in_consensus}-{haplotype_letter}.fasta')
+                new_fasta_filepath = os.path.join(summary_dir, new_fasta_filename)
+                
+                # Relocate the '-{medaka_count}-RiC{reads_in_consensus}.fasta' file to the summary_dir
+                shutil.move(fasta_file, new_fasta_filepath)
+                
+                # Rename the new_fastq_filepath file to include the haplotype_letter
+                new_fastq_filepath_1 = new_fasta_filepath.replace('.fasta','.fastq')
+                new_fastq_filepath_1 = new_fastq_filepath_1.replace(f'{summary_dir}',f'{fastq_dir}')
+                os.rename(new_fastq_filepath, new_fastq_filepath_1)
+                new_base_name = f'{base_name}-{medaka_count}-{haplotype_letter}'
+                stats = [new_base_name, sequence_length, reads_in_consensus, medaka_count]
+                stats_df.loc[len(stats_df)] = stats
+                for i, haplotype_fasta_file in enumerate(individual_fasta_files):
+                    # Rename the individual fasta files to use the base_name but replace '_NGSequenceID' with '-{medaka_count}-RiC{reads_in_consensus}-A.fasta' or '-{medaka_count}-RiC{reads_in_consensus}-B.fasta'
+                    haplotype_letter = chr(66+i)
+                    new_fasta_filename = base_name.replace('_NGSequenceID', f'-{medaka_count}-RiC{reads_in_consensus}-{haplotype_letter}.fasta')
+                    new_fasta_filepath = os.path.join(summary_dir, new_fasta_filename)
+                    
+                    # Relocate the '-{medaka_count}-RiC{reads_in_consensus}-A.fasta' & '-{medaka_count}-RiC{reads_in_consensus}-B.fasta' files to the summary_dir
+                    shutil.move(haplotype_fasta_file, new_fasta_filepath)
+                    
+                    # Duplicate the new_fastq_filepath file to include the the updated haplotype_letter
+                    new_fastq_filepath_i = new_fasta_filepath.replace('.fasta','.fastq')
+                    new_fastq_filepath_i = new_fastq_filepath_i.replace(f'{summary_dir}',f'{fastq_dir}')
+                    
+                    # Duplicate and rename the file
+                    shutil.copy(new_fastq_filepath_1, new_fastq_filepath_i)
+                    new_base_name = f'{base_name}-{medaka_count}-{haplotype_letter}'
+                    stats = [new_base_name, sequence_length, reads_in_consensus, medaka_count]
+                    stats_df.loc[len(stats_df)] = stats
+            elif haplotype_count == 2:
+                for i, haplotype_fasta_file in enumerate(individual_fasta_files):
+                    # Rename the individual fasta files to use the base_name but replace '_NGSequenceID' with '-{medaka_count}-RiC{reads_in_consensus}-A.fasta' or '-{medaka_count}-RiC{reads_in_consensus}-B.fasta'
+                    haplotype_letter = chr(65+i)
+                    new_fasta_filename = base_name.replace('_NGSequenceID', f'-{medaka_count}-RiC{reads_in_consensus}-{haplotype_letter}.fasta')
+                    new_fasta_filepath = os.path.join(summary_dir, new_fasta_filename)
+                    
+                    # Relocate the '-{medaka_count}-RiC{reads_in_consensus}-A.fasta' & '-{medaka_count}-RiC{reads_in_consensus}-B.fasta' files to the summary_dir
+                    shutil.move(haplotype_fasta_file, new_fasta_filepath)
+                    
+                    # Duplicate the new_fastq_filepath file to include the the updated haplotype_letter
+                    new_fastq_filepath_i = new_fasta_filepath.replace('.fasta','.fastq')
+                    new_fastq_filepath_i = new_fastq_filepath_i.replace(f'{summary_dir}',f'{fastq_dir}')
+                    shutil.copy(new_fastq_filepath, new_fastq_filepath_i)
+                    new_base_name = f'{base_name}-{medaka_count}-{haplotype_letter}'
+                    stats = [new_base_name, sequence_length, reads_in_consensus, medaka_count]
+                    stats_df.loc[len(stats_df)] = stats
+                os.remove(new_fastq_filepath)
+            else:
+                # Rename the consensus_reference_#.fasta & fastq in current_sample_dir to use the base_name but replace '_NGSequenceID' with '-{medaka_count}-RiC{reads_in_consensus}.fasta'
+                new_fasta_filename = base_name.replace('_NGSequenceID', f'-{medaka_count}-RiC{reads_in_consensus}.fasta')
+                new_fasta_filepath = os.path.join(summary_dir, new_fasta_filename)
+                
+                # Relocate the -{medaka_count}-RiC{reads_in_consensus}.fasta & .fastq file to the summary_dir
+                shutil.move(fasta_file, new_fasta_filepath)
+                new_base_name = f'{base_name}-{medaka_count}'
+                stats = [new_base_name, sequence_length, reads_in_consensus, medaka_count]
+                stats_df.loc[len(stats_df)] = stats
+            medaka_count += 1
+        
+        # Write the tab separated Summary.txt file based on the columns in the stats dataframe
+        summary_name = summary_dir.split("/")[-1]
+        stats_df.to_csv(os.path.join(summary_dir, f'{summary_name}.txt'), sep='\t', index=False)
+        
+        # Get a list of all fasta files in the directory
+        fasta_files = glob.glob(f"{summary_dir}/*.fasta")
+        
+        # Create the new combined fasta file
+        combined_fasta_filename = os.path.join(summary_dir, f"{summary_name}.fasta")
+        with open(combined_fasta_filename, 'w') as combined_fasta_file:
+            for fasta_file in fasta_files:
+                # Get the base name of the file (strip off the .fasta extension)
+                new_id = os.path.basename(fasta_file).replace('.fasta', '')
+        
+                # Parse the fasta file and update the id of each record
+                records = list(SeqIO.parse(fasta_file, 'fasta'))
+                for record in records:
+                    record.id = new_id
+                    record.description = ''
+        
+                # Write the updated records to the combined fasta file
+                SeqIO.write(records, combined_fasta_file, 'fasta')
+                
+def main(args):
+    # Set the path to the folder containing the fastq files
+    percent_system_use = float(args.percent_system_use)/100 if args.percent_system_use else 0.8
+    input_dir = args.input_dir if args.input_dir else os.path.dirname(os.path.realpath(__file__))
+    mycomap_summarize_ngsid_dir(input_dir)
+    print('PASS: Successfully summarized NGSequenceID Folder for MycoMap upload')
 
 if __name__ == "__main__":
+    # Global environment_dir
     environment_dir = ""
     environment_cmd_prefix = ""
     environment_dir = check_os()
@@ -195,7 +172,7 @@ if __name__ == "__main__":
     
     # Parse user arguments
     parser = argparse.ArgumentParser(description="Process NGSpeciesID source folder.")
-    parser.add_argument('-s','--source_folder', type=str, help='Path to the source folder')
+    parser.add_argument('-i','--input_dir', type=str, help='Path to the NGSpeciesID source folder')
     parser.add_argument('-p','--percent_system_use', type=str, help='Percent system use written as integer.')
     args = parser.parse_args()    
     main(args)
